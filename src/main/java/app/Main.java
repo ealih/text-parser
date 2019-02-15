b6c5e0da394a1b046404ba6f1ee5070c88c2373a
Esed <esed.alih@gmail.com>
2019-02-15 15:16:02 +0100

    Fix typos

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args) {
         //String sample = "{This is my {homework|assignment}.}";
         //String sample = "{ Today is { Monday { and it's working } | Friday { and it's { relaxing | easy} } day }.}";
-        String sample = "{ Today is { Monday { and it's working } | Friday { and it's { relaxing | easy} } day }. Tonight is { baseball | football } game. }";
+        //String sample = "{ Today is { Monday { and it's working } | Friday { and it's { relaxing | easy} } day }. Tonight is { baseball | football } game. }";
         //String sample = "{ Today is { Monday { and it's working } | Friday { and it's { relaxing | easy} } day }. Tonight is { baseball | football } game. This is my {homework|assignment}.}";
 
         if(args.length == 0) {
             System.out.println("Usage: app <text>");
-            //System.exit(1);
+            System.exit(1);
         }
 
         App app = new App();
-        List<String> combinations = app.parse(sample);
+        List<String> combinations = app.parse(args[0]);
 
         System.out.println(String.format("Combinations (%d):", combinations.size()));
 
